e91ebc309b5c6019a5261170b5eb8c1f2f3cbc59
praveenmoorthy603-ship-it <praveenmoorthy603@gmail.com>
2026-05-26 13:38:01 +0530

    changed mysql to sqlite

--- Djangoproject/settings.py
+++ Djangoproject/settings.py
@@ -83,17 +83,23 @@ WSGI_APPLICATION = 'Djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'django_db',
+#         'USER': 'root',
+#         'PASSWORD': 'Praveensql123',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_db',
-        'USER': 'root',
-        'PASSWORD': 'Praveensql123',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
